5e84cc918e5d3903941fd6154fad3ea6e5079d24
Callum Flack <callumflack@gmail.com>
2021-02-27 11:17:18 +1000

    Update ReportContent.js

--- components/ReportContent.js
+++ components/ReportContent.js
@@ -1,18 +1,24 @@
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
 // https://github.com/vercel/next.js/discussions/11291#discussioncomment-4303
 const componentList = {
   MyComponent: dynamic(() => import(`../../components/my-component`)),
-  OtherComponent: dynamic(() => import(`../../components/other-component`))
-}
+  OtherComponent: dynamic(() => import(`../../components/other-component`)),
+};
 
-export default function ReportContent({data}) {
-  const components = data.map((d) => componentList[d]))
-  return <>
-    {components.map(Component => {
-      return <li>
-       <Component />
-      </li>
-    })}
-  </>
+// eslint-disable-next-line react/prop-types
+export default function ReportContent({ data }) {
+  // eslint-disable-next-line react/prop-types
+  const components = data.map((d) => componentList[d]);
+  return (
+    <>
+      {components.map((Component, index) => {
+        return (
+          <li key={index}>
+            <Component />
+          </li>
+        );
+      })}
+    </>
+  );
 }
